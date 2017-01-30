d04dc95ef39d8e761dbf2b29be28c9cc9edd0c73
andrey9828 <aaa73731919@gmail.com>
2017-01-30 10:38:41 +0200

    Added generation of mines

--- mines/static/main.js
+++ mines/static/main.js
@@ -28,6 +28,43 @@ window.onload = function () {
     }
     grid.setAttribute("border", "1");
     grid.setAttribute("id", "grid");
+
+
+    var minesCoord = [];
+
+    function generate_mines() {
+      if (document.getElementById('grid')) {
+        var grid = document.getElementById('grid');
+        var randTr;
+        var randTd;
+        var value = '';
+        for (var t = 0; t < mines; t++) {
+          randTr = Math.floor(Math.random() * (15 - 0 + 1)) + 0;
+          randTd = Math.floor(Math.random() * (15 - 0 + 1)) + 0;
+          value = randTr.toString() + ',' + randTd.toString();
+
+          if (minesCoord.indexOf(value) == -1) {
+            minesCoord.push(value);
+
+            for (var i in grid.children) {
+              if (grid.children.hasOwnProperty(i) && i == randTr) {
+                for (var j in grid.children[i].children) {
+                  if (grid.children[i].children.hasOwnProperty(j) && j == randTd) {
+                    grid.children[i].children[j].removeAttribute("style");
+                    grid.children[i].children[j].className = 'mine';
+                    grid.children[i].children[j].style.background = 'grey';
+                  }
+                }
+              }
+            }
+          } else {
+            t--;
+          }
+        }
+      }
+    }
+
+    generate_mines();
   }
   gridInit();
 };
