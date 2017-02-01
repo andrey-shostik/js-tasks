8f98abeb076a80e511d920eaa00d642e11991d40
andrey9828 <aaa73731919@gmail.com>
2017-02-01 11:20:59 +0200

    Added es6 task 17

---
+++ es6/TemplateStrings/1/theToolingTeam.js
@@ -0,0 +1,9 @@
+const teamName = "tooling"
+const people = [{name: "Jennie", role: "senior"},
+                {name: "Ronald", role: "junior"},
+                {name: "Martin", role: "senior"},
+                {name: "Anneli", role: "junior"}]
+
+let message = `Hi ${people[0].name} you are ${people[0].role} `
+
+console.log(message)
