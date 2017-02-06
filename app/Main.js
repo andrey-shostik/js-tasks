aac2f3bc6ac151f811208ee7569edcae50395275
andrey9828 <aaa73731919@gmail.com>
2017-02-06 15:45:22 +0200

    Updated entry point

--- app/Main.js
+++ app/Main.js
@@ -1,1 +1,7 @@
 require('./Main.scss');
+
+window.onload = function () {
+  var model = new(require('../app/models/MinesweeperModel'));
+  var controller = new(require('../app/controllers/MinesweeperController'))(model);
+  var view = new(require('../app/views/MinesweeperView'))(model, controller);
+};
