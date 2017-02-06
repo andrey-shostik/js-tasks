84568bee5a9a580882a1dca89ca3c5380afc301a
andrey9828 <aaa73731919@gmail.com>
2017-02-06 15:57:00 +0200

    Added console Controller

--- app/Main.js
+++ app/Main.js
@@ -1,7 +1,9 @@
+'use strict';
 require('./Main.scss');
 
 window.onload = function () {
   var model = new(require('../app/models/MinesweeperModel'));
   var controller = new(require('../app/controllers/MinesweeperController'))(model);
-  var view = new(require('../app/views/MinesweeperView'))(model, controller);
+  this.game = new(require('../app/controllers/ConsoleController'))(model);
+  var view = new(require('../app/views/MinesweeperView'))(model, controller, this.game);
 };
